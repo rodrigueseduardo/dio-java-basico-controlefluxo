e0a2c11b6888b6f9ec1764e8578b8f8b64a1c548
Eduardo Rodrigues <eduxdudu@icloud.com>
2024-05-08 13:44:28 -0300

    try-catch, exceptions

---
+++ src/FormatadorCepExemplo.java
@@ -0,0 +1,18 @@
+public class FormatadorCepExemplo {
+    public static void main(String[] args) {
+        try {
+            String cepFormatado = formatarCep("2376506");
+            System.out.println(cepFormatado);
+        } catch (CepInvalidoException e) {
+            // TODO Auto-generated catch block
+            System.out.println("O ceo não corresponde com as regras de negócio.");
+        }
+    }
+    static String formatarCep(String cep) throws CepInvalidoException{
+        if(cep.length() != 8)
+        throw new CepInvalidoException();
+
+        //simulando um cep formatado
+        return "23.765-064";
+    }
+}
